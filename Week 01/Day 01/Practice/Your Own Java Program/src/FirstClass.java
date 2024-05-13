766707221243f8c27f4a59d74f244c396a062429
abderrahmenz <abderrahmenzarrougui7@gmail.com>
2024-05-13 14:28:36 +0100

    My first commit

---
+++ Week 01/Day 01/Practice/Your Own Java Program/src/FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("My name is Coding Dojo\n");
+        System.out.print("I am 100 years old\n");
+        System.out.print("My hometown is Burkbank, CA");
+    }
+}
